058a44468ff31e1a92ca08b573c7786c2e9021a3
Amrutha2103 <amrutha.ananthan@outlook.com>
2023-11-01 14:43:37 +0000

    Style django forms with bootstrap

--- blog/models.py
+++ blog/models.py
@@ -3,24 +3,18 @@ from django.contrib.auth.models import User
 from django.urls import reverse
 # from cloudinary.models import CloudinaryField
 
-
-
 # Create your models here.
 
+
 class Post(models.Model):
-    title= models.CharField(max_length=255)
-    title_tag= models.CharField(max_length=255, default=" Click and Visit")
+    title = models.CharField(max_length=255)
+    title_tag = models.CharField(max_length=255)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     body = models.TextField()
 
     def _str_(self):
-        return self.title + ' | ' + self.author  
+        return self.title + ' | ' + self.author
 
     def get_absolute_url(self):
-        #return reverse('article-detail', args=(str(self.id)))
+        # return reverse('article-detail', args=(str(self.id)))
         return reverse('home')
-
-
-
-
-
--- blog/views.py
+++ blog/views.py
@@ -1,12 +1,14 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView, CreateView
 from .models import Post
+from .forms import PostForm
 
 # Create your views here.
 
-#def home(request):
+# def home(request):
 #    return render(request, 'home.html', {})
-    
+
+
 class HomeView(ListView):
     model = Post
     template_name = 'home.html'
@@ -16,8 +18,10 @@ class ArticleDetailView(DetailView):
     model = Post
     template_name = 'article_details.html'
 
+
 class AddPostView(CreateView):
     model = Post
+    form_class = PostForm
     template_name = 'add_post.html'
-    fields = '_all_'
-
+    # fields = '_all_'
+    # field = ('title', 'body')
