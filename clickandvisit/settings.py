7679fa9a3d7058fa3c3bcf40b706ceb8a1b30748
Amrutha2103 <amrutha.ananthan@outlook.com>
2023-11-03 20:08:26 +0000

    Add allowed host field in settings.py

--- clickandvisit/settings.py
+++ clickandvisit/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-amrutha2103-clickandvis-rycwnn5bxj7.ws-eu105.gitpod.io', 'localhost']
+ALLOWED_HOSTS = ['8000-amrutha2103-clickandvis-dex062mptn4.ws-eu105.gitpod.io', 'localhost']
 
 # Application definition
 
