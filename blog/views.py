962be946a71fa97a673dcbac23051b2739190ea2
Amrutha2103 <amrutha.ananthan@outlook.com>
2023-11-17 14:55:56 +0000

    Add blog categories

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
-from .models import Post
+from .models import Post, Category
 from .forms import PostForm, EditForm
 from django.urls import reverse_lazy
 
@@ -27,6 +27,14 @@ class AddPostView(CreateView):
     form_class = PostForm
     template_name = 'add_post.html'
     # fields = '_all_'
+    # field = ('title', 'body')
+
+
+class AddCategoryView(CreateView):
+    model = Category
+    #form_class = PostForm
+    template_name = 'add_category.html'
+    fields = '_all_'
     # field = ('title', 'body')
 
 
